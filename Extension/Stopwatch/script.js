34db589b4b53e3791c2b3ec377bfcf5ebdc859dd
Lilian <pade.lilian@gmail.com>
2020-09-22 19:12:40 +0200

    add stopwatch

---
+++ Extension/Stopwatch/script.js
@@ -0,0 +1,76 @@
+var Stopwatch = function(elem, options){
+
+  var timer = document.getElementById("timer");
+
+  var startButton = document.getElementById("startbutton");
+  startButton.addEventListener("click", function(event) {
+    start();
+    event.preventDefault();
+  });
+
+  var stopButton = document.getElementById("stopbutton");
+  stopButton.addEventListener("click", function(event) {
+    stop();
+    event.preventDefault();
+  });
+
+  var resetButton = document.getElementById("resetbutton");
+  resetButton.addEventListener("click", function(event) {
+    reset();
+    event.preventDefault();
+  });
+
+  var offset,
+      clock,
+      interval;
+
+  options = options || {};
+  options.delay = options.delay || 1;
+
+  reset();
+
+  function start() {
+    if (!interval) {
+      offset   = Date.now();
+      interval = setInterval(update, options.delay);
+    }
+  }
+
+  function stop() {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+  }
+
+  function reset() {
+    clock = 0;
+    render();
+  }
+
+  function update() {
+    clock += delta();
+    render();
+  }
+
+  function render() {
+    timer.innerHTML = clock/1000;
+  }
+
+  function delta() {
+    var now = Date.now(),
+        d   = now - offset;
+
+    offset = now;
+    return d;
+  }
+};
+
+var elems = document.getElementsByClassName("stopwatch");
+
+var elem = document.getElementById("my-stopwatch");
+var timer = new Stopwatch(elem);
+
+for (var i=0, len=elems.length; i<len; i++) {
+  new Stopwatch(elems[i]);
+}

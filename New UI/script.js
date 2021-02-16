d6d1d6856eef3adfc9c0078d97d4fe92a3af4ff2
LLinoor <lillesully@gmail.com>
2021-02-16 23:17:51 +0100

    new ui

--- New UI/script.js
+++ New UI/script.js
@@ -1,67 +1,45 @@
 var currentZone = window.location.search.substr(1)
 externalTimezone = false
-multiplesClock = false
+
+const Http = new XMLHttpRequest()
+if (currentZone == ""){
+    url = "http://worldtimeapi.org/api/ip.json"
+}
+else if(currentZone.includes("GMT")){
+    if(currentZone.includes("+")){
+        currentZone = currentZone.replace("+", "-")
+    }
+    else if(currentZone.includes("-")){
+        currentZone = currentZone.replace("-", "+")
+    }
+    url = "http://worldtimeapi.org/api/timezone/" + currentZone
+    externalTimezone = true
+}
+else {
+    url = "http://worldtimeapi.org/api/timezone/" + currentZone
+    externalTimezone = true
+}
+Http.open("GET", url)
+Http.send()
+
+response = 0
+isConnected = false
+
+Http.onreadystatechange=function(){
+   if(this.readyState==4 && this.status==200){
+    const json = Http.responseText
+    response = JSON.parse(json)
+    document.getElementById('info').innerHTML = 'Connected to <a href="http://worldtimeapi.org/" rel="noreferrer">satellite</a> (~1sec) 🛰️'
+    isConnected = true
+   }
+   else if(this.status!=200 || this.readyState==0){
+    document.getElementById('info').innerHTML = 'Unable to get the satellite time, switch to local time. ❌'
+   }
+}
+
 counter = 0
-
-function getTime(currentZone){
-    Http = new XMLHttpRequest()
-    if (currentZone == ""){
-        url = "https://worldtimeapi.org/api/ip.json"
-    }
-    else if(currentZone.includes("GMT")){
-        if(currentZone.includes("+")){
-            currentZone = currentZone.replace("+", "-")
-        }
-        else if(currentZone.includes("-")){
-            currentZone = currentZone.replace("-", "+")
-        }
-        url = "https://worldtimeapi.org/api/timezone/" + currentZone
-        externalTimezone = true
-    }
-    else {
-        url = "https://worldtimeapi.org/api/timezone/" + currentZone
-        externalTimezone = true
-    }
-    Http.open("GET", url)
-    Http.send()
-
-    response = 0
-    isConnected = false
-
-    Http.onreadystatechange=function(){
-    if(this.readyState==4 && this.status==200){
-        console.log(Http.responseURL)  
-        const json = Http.responseText
-        response = JSON.parse(json)
-        document.getElementById('info').innerHTML = 'Connected to <a href="http://worldtimeapi.org/" rel="noreferrer">satellite</a> (~1sec) 🛰️'
-        isConnected = true
-    }
-    else if(this.status!=200 || this.readyState==0){
-        document.getElementById('info').innerHTML = 'Unable to get the satellite time, switch to local time. ❌'
-    }
-    }
-}
-
-function createClock(id){
-    clock = document.createElement("span")
-    clock.className = "horloge"
-    clock.id = id
-    document.body.appendChild(clock)
-}
-
-if(currentZone.includes("&")){
-    multiplesClock = true
-    timezones = currentZone.split("&")
-    for(i = 0; i < timezones.length; i++){
-        createClock(timezones[i])
-        getTime(timezones[i])
-        clock.textContent = response["datetime"]
-    }
-}
-if(multiplesClock == false){
-    getTime(currentZone)
-    createClock("ok")
-}
+utc_offset = response["utc_offset"]
+ok = +1
 
 function refresh(){
    counter = counter + 1
@@ -70,13 +48,16 @@ function refresh(){
 }  
 
 function showDate() {
-    if(multiplesClock == true){
-        return
-    }
-    else if(isConnected == true){
+    if(isConnected == true && externalTimezone == true){
         x = response["datetime"].slice(0, -6)
         date = new Date(x)
         date.setSeconds(date.getSeconds() + counter)
+    }
+    else if(isConnected == true){
+        unix =  response["unixtime"]*(60*ok)
+        unix = response["unixtime"] + counter
+        var milliseconds = unix * 1000
+        date = new Date(milliseconds)
     }
     else{
         var date = new Date()
@@ -88,7 +69,7 @@ function showDate() {
    if( m < 10 ){ m = '0' + m; }
    if( s < 10 ){ s = '0' + s; }
    var time = h + ':' + m + ':' + s
-   clock.textContent = time
+   document.getElementById('horloge').innerHTML = time;
    refresh();
 }
 
